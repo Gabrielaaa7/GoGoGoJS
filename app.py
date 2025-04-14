e2be664b05b68f59a3d3936a229561274b158ded
Jakub Rosol <Kuba.Rosol@seznam.cz>
2025-04-14 20:21:38 +0200

    Good practice - commit

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 import json
 
-diagram = {
-    "class": "go.GraphLinksModel",
-    "nodeDataArray": [
-        {"key": 10, "text": "Python Node", "color": "pink"},
-        {"key": 20, "text": "Generated", "color": "lightyellow"},
-    ],
-    "linkDataArray": [
-        {"from": 10, "to": 20}
-    ]
+nodes = [
+    {"key": 1, "name": "Miton C", "type": "company"},
+    {"key": 2, "name": "Munchained SE", "type": "company"},
+    {"key": 3, "name": "Martin Hála", "type": "person"},
+    {"key": 4, "name": "Note about Miton C", "type": "note", "relatedTo": 1}
+]
+
+links = [
+    {"from": 1, "to": 2},
+    {"from": 1, "to": 3},
+    {"from": 4, "to": 1, "category": "noteLink"}
+]
+
+data = {
+    "nodes": nodes,
+    "links": links
 }
 
-with open("static/data/diagram.json", "w") as f:
-    json.dump(diagram, f, indent=2)
+with open("static/data/graph.json", "w") as f:
+    json.dump(data, f, indent=2)
